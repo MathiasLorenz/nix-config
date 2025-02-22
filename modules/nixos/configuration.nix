ec56e5ca2e5833f8e8821a90c7d371eaa9c217ec
Mathias Lorenz <lorenz.mathias@gmail.com>
2025-02-22 10:15:43 +0100

    Set fish as default shell by starting it from bash. This ensures we still have a POSIX compliant shell (bash) to fall back on if things crap themselves

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -81,6 +81,21 @@
     isNormalUser = true;
     description = "Mathias Lorenz";
     extraGroups = [ "networkmanager" "wheel" ];
+
+    # set default shell to bash, which launches fish
+    # see: https://nixos.wiki/wiki/Fish
+    shell = pkgs.bash;
+  };
+
+  # use bash to start fish shell when logging in interactively
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
   };
 
   # Install firefox.
