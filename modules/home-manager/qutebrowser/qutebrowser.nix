ead9672431efa40e40dce8e0e05341923d332da6
Mathias Lorenz <lorenz.mathias@gmail.com>
2025-02-24 22:07:59 +0100

    Qutebrowser config as home-manager module

--- modules/home-manager/qutebrowser/qutebrowser.nix
+++ modules/home-manager/qutebrowser/qutebrowser.nix
@@ -11,6 +11,8 @@
     settings = {
       tabs = {
         position = "left";
+        select_on_remove = "last-used";
+        show_switching_delay = 2500;
       };
 
       colors = {
@@ -22,6 +24,31 @@
             };
           };
         };
+      };
+
+      # Not entirely sure what this does
+      completion.shrink = true;
+
+      # Re-open tabs from previous session
+      auto_save.session = true;
+
+      qt = {
+        highdpi = true;
+        args = [ "enable-features=VaapiVideoDecodeLinuxGL" ];
+      };
+
+      zoom.default = 120;
+
+      downloads.location.directory = "~/Downloads/";
+    };
+
+    keyBindings = {
+      normal = {
+        "tt" = "config-cycle tabs.show always switching";
+        "tsa" = "set tabs.show always";
+        "tsn" = "set tabs.show never";
+        "tsm" = "set tabs.show multiple";
+        "tss" = "set tabs.show switching";
       };
     };
   };
