33e182bcab523cafeec86b8a253852da1fa30b68
Mathias Lorenz <lorenz.mathias@gmail.com>
2025-02-25 16:11:05 +0100

    Set explicit theme for cursors and icons with home-manager module

--- hosts/claude/home.nix
+++ hosts/claude/home.nix
@@ -11,6 +11,7 @@ in
     ../../modules/home-manager/qutebrowser/qutebrowser.nix
     ../../modules/home-manager/git.nix
     ../../modules/home-manager/fuzzel.nix
+    ../../modules/home-manager/theme.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
---
+++ modules/home-manager/theme.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+{
+  # All is stolen from zanderhavgaard config
+  gtk = {
+    enable = true;
+
+    gtk3 = {
+      extraConfig = {
+        gtk-application-prefer-dark-theme = true;
+      };
+    };
+    gtk4 = {
+      extraConfig = {
+        gtk-application-prefer-dark-theme = true;
+      };
+    };
+
+    cursorTheme = {
+      name = "capitaine-cursors";
+      package = pkgs.capitaine-cursors;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    theme = {
+      name = "Arc-Dark";
+      package = pkgs.arc-theme;
+    };
+  };
+
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+  };
+
+  qt = {
+    enable = true;
+    platformTheme.name = "gtk";
+  };
+}
