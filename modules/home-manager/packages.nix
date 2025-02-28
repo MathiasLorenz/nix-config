154da075303be27b5500c75b59787e14e131f9b7
Mathias Lorenz <lorenz.mathias@gmail.com>
2025-02-28 15:37:11 +0100

    Remove commented out lines from packages.nix which are no longer of any possible value

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -70,24 +70,6 @@
     # Work stuff, could be its own nix file
     tenv
 
-    # rocm-cli for AMD gpus, see https://cgmb-rocm-docs.readthedocs.io/en/latest/ROCm_System_Managment/ROCm-SMI-CLI.html
-    # rocmPackages.rocm-smi
-    # Installed with paru instead as 'llama.cpp-hip(-git)' which comes with all the extras for GPU support
-    # The llama-cpp package is with CPU support only, so I'll build it from scratch instead
-    # llama-cpp
-    # rocmPackages.hip-common
-    # rocmPackages.hipblas
-    # rocmPackages.rocblas
-    # rocmPackages.rocm-runtime
-    # rocmPackages.rocm-smi
-    # rocmPackages.rocminfo
-    # rocmPackages.rocprofiler
-    # aocl-utils
-
-    # Using asdf-vm turned out to be a shit show, this worked surprisingly well
-    # beamMinimal25Packages.elixir_1_17
-    # wkhtmltopdf
-
     # Graphical applications
     bitwarden-desktop
     bitwarden-cli
@@ -103,25 +85,6 @@
     zig
     valgrind
     gdb
-
-    # Currently I can't get sway to work with home-manager
-    # The problem is that wlroots can't launch as it is looking for
-    # EGL extensions (OpenGL)
-    # Should be fixable... For now sway can be installed with pacman/paru
-    # nixGL https://github.com/nix-community/nixGL should definitly be able to solve the issue
-    # but I cannot get it to work....
-    # See 'GPU on non-NixOS systems' here https://nix-community.github.io/home-manager/
-    # This should be the same for all gpu powered programs (like ghostty I suppose)
-    # sway
-    # ghostty
-
-    # The sway 'downstream' dependencies can still be installed with home-manager
-    # swaybg
-    # swayidle
-    # swaylock # Even when using the correct password I can't unluck with this - it works if installed with pacman..
-    # waybar
-    # mako
-    # swayimg
 
     # Git stuff
     gh
