1095f04358b33297a92927d7c0fd2229b27944c1
MathiasLorenz <lorenz.mathias@gmail.com>
2025-03-03 09:07:24 +0100

    Comment about how to connect things with bluetooth

--- modules/nixos/bluetooth.nix
+++ modules/nixos/bluetooth.nix
@@ -1,5 +1,14 @@
 { ... }:
 {
+  # To pair, do:
+  # bluetoothctl
+  # scan on
+  # when you see mac address of device
+  # scan off
+  # yank mac address
+  # pair <mac-address>
+  # trust <mac-address>
+  # connect <mac-address>
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 }
