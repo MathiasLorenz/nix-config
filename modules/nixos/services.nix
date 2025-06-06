873e3a59c522c829447a8b0d25377e75ad128da3
MathiasLorenz <lorenz.mathias@gmail.com>
2025-06-06 22:55:48 +0200

    Try to add dnscrypt-proxy for encrypted dns

--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -5,6 +5,11 @@
     # Disable for chromecast if needed
     firewall.enable = true;
     nftables.enable = true;
+
+    nameservers = [
+      "127.0.0.1"
+      "::1"
+    ];
   };
 
   virtualisation = {
@@ -34,6 +39,30 @@
       enable = true;
       # Update database on an interval
       interval = "hourly";
+    };
+
+    # https://nixos.wiki/wiki/Encrypted_DNS
+    dnscrypt-proxy2 = {
+      enable = true;
+      # Settings reference:
+      # https://github.com/DNSCrypt/dnscrypt-proxy/blob/master/dnscrypt-proxy/example-dnscrypt-proxy.toml
+      settings = {
+        ipv6_servers = true;
+        require_dnssec = true;
+        # Add this to test if dnscrypt-proxy is actually used to resolve DNS requests
+        # query_log.file = "/var/log/dnscrypt-proxy/query.log";
+        sources.public-resolvers = {
+          urls = [
+            "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+            "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+          ];
+          cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
+          minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+        };
+
+        # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+        # server_names = [ ... ];
+      };
     };
   };
 }
