e90e0d69a1cffb9a1c150b7200005240099a1fa3
Mathias Lorenz <lorenz.mathias@gmail.com>
2026-05-23 23:21:51 +0200

    Try zen kernel package as that has bluetooth patch already in 7.0.9

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -4,5 +4,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
+  # Try zen kernel package as that has bluetooth patch for 7.0.9
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 }
