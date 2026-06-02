71beac243a638c9e0ecc0104b8743a1dae718cce
Mathias Lorenz <lorenz.mathias@gmail.com>
2026-06-02 18:42:14 +0200

    Add electron-39 to permittedInsecurePackages as upstream bitwarden-desktop is behind...

--- modules/nixos/programs.nix
+++ modules/nixos/programs.nix
@@ -32,4 +32,12 @@
       "postman"
       "claude-code"
     ];
+
+  # https://github.com/NixOS/nixpkgs/issues/521305
+  # bitwarden-desktop upstream has a dep on electron 39, which is EOL
+  # This allows the package to be built. When the above issue is fixed,
+  # this can be removed
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-39.8.10"
+  ];
 }
