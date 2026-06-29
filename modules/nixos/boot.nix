ae5e5daac47f329ee9c3309692ec82b0e534541a
Mathias Lorenz <lorenz.mathias@gmail.com>
2026-06-29 10:31:09 +0200

    Flake update and temp boot loader fix

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -7,4 +7,8 @@
   # boot.kernelPackages = pkgs.linuxPackages_latest;
   # Try zen kernel package as that has bluetooth patch for 7.0.9
   boot.kernelPackages = pkgs.linuxPackages_zen;
+
+  # temp fix for linux zen 7.0.12 not being found properly,
+  # see https://github.com/nixos/nixpkgs/issues/535850
+  system.boot.loader.kernelFile = "vmlinuz";
 }
